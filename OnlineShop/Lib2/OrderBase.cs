e31c1f508096aa964e17c8ea2d66c2e926e16e9d
Vitaliymartunyk1931 <martunyk987@gmail.com>
2023-09-19 09:53:00 +0300

    add home work

---
+++ OnlineShop/Lib2/OrderBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lib2
+{
+    public class OrderBase
+    {
+        public int OrderId { get; set; }
+        public decimal Discount { get; set; }
+    }
+
+    public class ItemBase
+    {
+        public int ItemId { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
